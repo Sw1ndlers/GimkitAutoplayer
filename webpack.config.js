bf5cf8dcf9ab3a6cca57c8a74d912695b82c01ee
Sw1ndlers <lisina2023@icloud.com>
2023-12-14 22:47:32 -0800

    Make obfuscation production only

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,39 @@
 const path = require("path")
 const CopyPlugin = require("copy-webpack-plugin")
-const WebpackObfuscator = require('webpack-obfuscator');
+const WebpackObfuscator = require("webpack-obfuscator")
 
-module.exports = {
-    entry: "./src/index.ts",
-    devtool: "inline-source-map",
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: "ts-loader",
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    resolve: {
-        extensions: [".tsx", ".ts", ".js"],
-        alias: {
-            "@root": path.resolve(__dirname, "src"),
-            "@functions": path.resolve(__dirname, "src/functions"),
+module.exports = (env) => {
+    return {
+        entry: "./src/index.ts",
+        devtool: "inline-source-map",
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: "ts-loader",
+                    exclude: /node_modules/,
+                },
+            ],
         },
-    },
-    output: {
-        filename: "index.js",
-        path: path.resolve(__dirname, "dist"),
-    },
-    plugins: [
-        new CopyPlugin({
-            patterns: [{ from: "extention", to: "./" }],
-        }),
-        new WebpackObfuscator ({
-            rotateStringArray: true
-        })
-    ],
+        resolve: {
+            extensions: [".tsx", ".ts", ".js"],
+            alias: {
+                "@root": path.resolve(__dirname, "src"),
+                "@functions": path.resolve(__dirname, "src/functions"),
+            },
+        },
+        output: {
+            filename: "index.js",
+            path: path.resolve(__dirname, "dist"),
+        },
+        plugins: [
+            new CopyPlugin({
+                patterns: [{ from: "extention", to: "./" }],
+            }),
+            env.production &&
+                new WebpackObfuscator({
+                    rotateStringArray: true,
+                }),
+        ],
+    }
 }
